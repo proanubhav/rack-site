9fb7497d99796201a3f73268dbcf3c99d725639d
Anubhav Gupta <anubhavgupta@gmail.com>
2025-08-31 18:05:34 +0530

    books-and-library-racks product listed

--- common/header.php
+++ common/header.php
@@ -82,7 +82,7 @@
                         <li><a href="medical-store-racks.php">Medical Store Racks</a></li>
                         <li><a href="electronic-display-racks.php">Electronic Display Racks</a></li>
                         <li><a href="textile-and-garment-racks.php">Textile &amp; Garment Racks</a></li>
-                        <li><a href="supermarket-racks.php">Books &amp; Library Racks</a></li>
+                        <li><a href="books-and-library-racks.php">Books &amp; Library Racks</a></li>
                         <li><a href="supermarket-racks.php">Bag Racks</a></li>
                         <li><a href="supermarket-racks.php">Footwear Racks</a></li>
                         <li><a href="supermarket-racks.php">Departmental Store Racks</a></li>
