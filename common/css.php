a8a324abb9f2903faefa364dceaca76f4cbf868a
Anubhav Gupta <proanubhav@gmail.com>
2026-02-11 17:02:45 +0530

    bug fix

--- common/css.php
+++ common/css.php
@@ -1,4 +1,14 @@
-  <link rel="stylesheet" href="assets/css/swiper-bundle.min.css" />
+<meta name="google-site-verification" content="-csooiiKYbENTldppI9DrbICbzpNR9xxyttlca-q70w" />
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z8FDMMYYD6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z8FDMMYYD6');
+</script>
+<link rel="stylesheet" href="assets/css/swiper-bundle.min.css" />
   <link rel="stylesheet" href="assets/css/jquery-ui.css" />
   <link rel="stylesheet" href="assets/css/boxicons.min.css" />
   <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
